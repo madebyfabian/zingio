9c531b23303dda021e7d58c1598b88827046e3ed
madebyfabian <beer.fabian@mail.de>
2022-11-20 12:51:41 +0100

    fix: add user feed

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
 	<div>
-		<h1>Hi!</h1>
-		<pre>{{ currentUser }}</pre>
+		<h1>Welcome back, {{ currentUser?.name }}</h1>
+
+		<section v-if="!currentUserFeedPending">
+			<Post
+				v-for="post of currentUserFeed"
+				:key="post.id"
+				:post="post"
+				type="feed"
+			/>
+		</section>
+
+		<LoadingIndicator v-else layout="fill" />
 	</div>
 </template>
 
@@ -9,8 +19,16 @@
 	import { useCurrentUserStore } from '@/stores/useCurrentUserStore'
 	const currentUserStore = useCurrentUserStore()
 	const currentUser = computed(() => currentUserStore.currentUser)
+	if (!currentUser.value) throw createError({ statusCode: 401 })
 
 	definePageMeta({
 		middleware: 'auth',
 	})
+
+	// Fetch `currentUserFeed`
+	const { data: currentUserFeed, pending: currentUserFeedPending } =
+		useLazyFetch('/api/currentUserFeed', {
+			// @ts-expect-error - this is a valid option
+			headers: useRequestHeaders(['cookie']),
+		})
 </script>
---
+++ server/api/currentUserFeed.ts
@@ -0,0 +1,33 @@
+import { useServerAuthUser } from '@/server/composables/useServerAuthUser'
+import { xata } from '@/server/lib/xata'
+
+const nonNullable = <T>(value: T): value is NonNullable<T> => {
+	return value !== null && value !== undefined
+}
+
+export default defineEventHandler(async event => {
+	console.log('handler:init')
+	const serverAuthUser = await useServerAuthUser(event)
+	if (!serverAuthUser) return sendError(event, createError({ statusCode: 401 }))
+
+	const usersFollowing = await xata.db.userFollowing
+		.select(['*', 'followsUser.*'])
+		.filter({ user: { authId: serverAuthUser.id } })
+		.getMany()
+
+	const usersFollowingIds = usersFollowing
+		.map(user => user.followsUser?.id)
+		.filter(nonNullable)
+	if (!usersFollowingIds.length) return []
+
+	const posts = await xata.db.post
+		.select(['*', 'authorUser.*'])
+		.filter({
+			authorUser: { id: { $any: usersFollowingIds } },
+			$notExists: 'isCommentOf',
+		})
+		.sort('createdAt', 'desc')
+		.getMany()
+
+	return posts
+})
