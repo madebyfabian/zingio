5de64d640d524a1305e445967897eadbd908768b
madebyfabian <beer.fabian@mail.de>
2022-11-23 00:26:54 +0100

    fix: add ability to change handle, also add validation server-side

--- pages/account/settings.vue
+++ pages/account/settings.vue
@@ -1,19 +1,43 @@
 <template>
 	<form @submit.prevent="handleSave">
-		<div class="flex justify-between items-center">
-			<h1>Settings</h1>
-			<button type="submit">Save</button>
+		<div>
+			<div class="mb-8 flex justify-between items-center">
+				<h1>Settings</h1>
+				<button type="submit">Save</button>
+			</div>
+		</div>
+		<h2 class="my-4">Profile</h2>
+		<div class="my-4" v-if="state.error === true">
+			Error saving. Make sure your @handle is 3-20 chars. Only letters (a-z,
+			A-Z), numbers, dash, dot and underscores are allowed.
 		</div>
 
-		<label class="mt-4 block">
-			<span>Name</span>
-			<input v-model="currentUserInputState.name" />
-		</label>
+		<div
+			class="my-4"
+			v-if="typeof state.error === 'string' && state.error.length > 0"
+		>
+			{{ state.error }}
+		</div>
+
+		<div class="my-4" v-if="state.success">Successfull saved!</div>
+
+		<div class="flex gap-6 flex-col">
+			<label class="block w-80">
+				<span class="text-sm block mb-1 text-gray-500">Name</span>
+				<input v-model="currentUserInputState.name" class="w-full" required />
+			</label>
+
+			<label class="block w-80">
+				<span class="text-sm block mb-1 text-gray-500">@handle</span>
+				<input v-model="currentUserInputState.handle" class="w-full" required />
+			</label>
+		</div>
 	</form>
 </template>
 
 <script setup lang="ts">
 	import { useCurrentUserStore } from '@/stores/useCurrentUserStore'
+	import { Base } from '../../.nuxt/components'
 	const currentUserStore = useCurrentUserStore()
 	const currentUser = computed(() => currentUserStore.currentUser)
 	if (!currentUser.value) throw createError({ statusCode: 401 })
@@ -26,11 +50,20 @@
 		title: `Settings`,
 	})
 
+	const state = reactive({
+		error: null as boolean | string | null,
+		success: null as boolean | null,
+	})
+
 	const currentUserInputState = reactive({
 		name: currentUser.value.name,
+		handle: currentUser.value.handle,
 	})
 
 	const handleSave = async () => {
+		state.error = null
+		state.success = null
+
 		const { data, error } = await useFetch('/api/currentUser', {
 			method: 'POST',
 			// @ts-expect-error - this is a valid option
@@ -40,10 +73,18 @@
 					id: currentUser.value?.id,
 					authId: currentUser.value?.authId,
 					name: currentUserInputState.name,
+					handle: currentUserInputState.handle,
 				},
 			},
 		})
-		if (error.value || !data.value) return console.error(error)
+		if (error.value || !data.value) {
+			if (error.value?.data.statusCode === 409)
+				state.error = 'This @handle is already taken.'
+			else state.error = true
+			state.success = false
+			return console.error(error)
+		}
+		state.success = true
 
 		// If successful, update the user in the store
 		currentUserStore.fetchCurrentUser()
--- server/api/currentUser.post.ts
+++ server/api/currentUser.post.ts
@@ -9,7 +9,13 @@ export default defineEventHandler(async event => {
 			currentUser: z.object({
 				id: z.string(),
 				authId: z.string(),
-				name: z.string().optional(),
+				name: z.string().trim().min(1).max(32),
+				handle: z
+					.string()
+					.trim()
+					.min(3)
+					.max(20)
+					.regex(/^[a-zA-Z0-9\_\-\.]+$/),
 			}),
 		})
 	)
@@ -18,9 +24,27 @@ export default defineEventHandler(async event => {
 	if (serverAuthUser.id !== body.currentUser.authId)
 		return sendError(event, createError({ statusCode: 403 }))
 
+	// First get existing user
+	const existingUser = await xata.db.user
+		.select(['*'])
+		.filter({ authId: serverAuthUser.id })
+		.getFirstOrThrow()
+
+	const hasChangedHandle = existingUser.handle !== body.currentUser.handle
+	if (hasChangedHandle) {
+		// Check if handle is already taken
+		const existingUserWithHandle = await xata.db.user
+			.select(['*'])
+			.filter({ handle: body.currentUser.handle })
+			.getFirst()
+		if (existingUserWithHandle)
+			return sendError(event, createError({ statusCode: 409 }))
+	}
+
 	const updatedRecord = await xata.db.user.update({
 		id: body.currentUser.id,
 		name: body.currentUser.name,
+		handle: body.currentUser.handle,
 		updatedAt: new Date(),
 	})
 	if (!updatedRecord) return sendError(event, createError({ statusCode: 500 }))
