d2f2479c78847a03a2609beaa65eaa0948d1c382
madebyfabian <beer.fabian@mail.de>
2022-11-24 11:59:09 +0100

    fix: prevent redirect to `/` when user already authenticated

--- app.vue
+++ app.vue
@@ -7,11 +7,15 @@
 
 <script lang="ts" setup>
 	const supabaseAuth = useSupabaseAuthClient()
+	const authUser = useSupabaseUser()
 
 	onMounted(() => {
 		supabaseAuth.auth.onAuthStateChange(async (event, session) => {
 			switch (event) {
 				case 'SIGNED_IN':
+					// If there already is a user, don't redirect.
+					if (authUser.value?.id) break
+
 					// Wait for a bit to make sure the session was synced with server. (Ugh, Bug or smth).
 					await new Promise(resolve => setTimeout(resolve, 250))
 					navigateTo('/')
