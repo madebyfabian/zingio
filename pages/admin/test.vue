0cb572a4480d1d75f08bfaf366bb5bba1a0b14bd
madebyfabian <beer.fabian@mail.de>
2022-11-27 12:06:26 +0100

    fix: added temporary test routes

---
+++ pages/admin/test.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<pre>{{ res }}</pre>
+	</div>
+</template>
+
+<script setup lang="ts">
+	const res = await useFetch('/api/v2/admin/test', {
+		headers: useRequestHeaders(['cookie']) as Record<string, any>,
+	})
+</script>
