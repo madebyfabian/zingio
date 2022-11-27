7ee50237dda4889c9b3f42e5f96248162e6fc199
madebyfabian <beer.fabian@mail.de>
2022-11-27 12:28:54 +0100

    fix: without `serverSupabaseUser`, nothing else

--- server/api/v2/admin/test.ts
+++ server/api/v2/admin/test.ts
@@ -1,10 +1,6 @@
-import { serverSupabaseUser } from '#supabase/server'
-
 export default defineEventHandler(async event => {
 	const startTime = +new Date()
 	console.log(startTime, 'start')
-	const serverAuthUser = await serverSupabaseUser(event)
-	if (!serverAuthUser) return sendError(event, createError({ statusCode: 401 }))
 
 	console.log(+new Date(), 'end', { took: +new Date() - startTime })
 	return true
